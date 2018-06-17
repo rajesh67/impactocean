ac5ce985dc4e7b78377eaf208f8211cddfe8b8d3
rajesh67 <rajeshmeena.iitkgp@gmail.com>
2018-06-17 14:43:17 +0530

    Firstt commit

---
+++ webapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+# Create your views here.
+
+class HomeView(View):
+
+	def get(self, request, *args, **kwargs):
+		return render(request, 'index.html', {})
