ece042d8b537f2ba66e662003cf6d69d80d7d268
rajesh67 <rajeshmeena.iitkgp@gmail.com>
2018-10-28 16:41:09 +0530

    Update settings.py

--- impactocean/settings.py
+++ impactocean/settings.py
@@ -26,7 +26,7 @@ if 'DJANGO_DEBUG_FALSE' in os.environ:
 else:
     DEBUG = True
     SECRET_KEY = 'insecure-key-for-dev'
-    ALLOWED_HOSTS = ['localhost', 'impactocean.herokuapp.com','127.0.0.1']
+    ALLOWED_HOSTS = []
 
 
 # Application definition
